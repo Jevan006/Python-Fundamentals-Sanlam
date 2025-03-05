390b12592ee76788314f87cb3d829021cafe138c
Jevan006 <jevan.peters@sanlam.co.za>
2025-03-05 15:25:36 +0200

    Day_11

---
+++ Day_09/Day_10/cool.py
@@ -0,0 +1,19 @@
+def to_upper_case(text):
+    """
+    This is beautiful unction that returns uppercase value with wave emoji
+    """
+    return text.upper() + "  👋"
+
+
+print(to_upper_case("Jamie"))
+
+# Only when cool.py execute -> Jamie
+# dunder variables
+if __name__ == "__main__":
+print(__name__)
+print (to_upper_case("Jamie"))
+
+# self-execution - __main__
+# From another filr - __name__ -> file name
+
+# sum()
---
+++ Day_09/Day_10/fun.py
@@ -0,0 +1,16 @@
+from Day_09.Day_10.cool import to_upper_case
+
+
+def powerful(n):
+    return n * n
+
+
+print(__name__)  # fun | __main__
+
+
+def main():
+    print(to_upper_case("Siya"))
+
+
+if __name__ == "__main__":
+    main()
